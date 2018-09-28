2728c187d83cec9aea1512dce2a207a2783cb5a5
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2018-09-28 09:17:10 +0100

    [PREVIEW] RIA-487 shared app service plan (#10)
    * use app service plan
    
    * RIA-487 shared App Service Plan

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,14 +1,23 @@
 provider "azurerm" {}
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location}"
-  tags     = "${merge(var.common_tags, map("lastUpdated", "${timestamp()}"))}"
+locals {
+
+  preview_resource_group_name     = "${var.product}-${var.component}-${var.env}"
+  non_preview_resource_group_name = "${var.product}-${var.env}"
+  resource_group_name             = "${var.env == "preview" || var.env == "spreview" ? local.preview_resource_group_name : local.non_preview_resource_group_name}"
+
+  preview_app_service_plan        = "${var.product}-${var.component}-${var.env}"
+  non_preview_app_service_plan    = "${var.product}-${var.env}"
+  app_service_plan                = "${var.env == "preview" || var.env == "spreview" ? local.preview_app_service_plan : local.non_preview_app_service_plan}"
+
+  preview_vault_name              = "${var.raw_product}-aat"
+  non_preview_vault_name          = "${var.raw_product}-${var.env}"
+  key_vault_name                  = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
 }
 
 data "azurerm_key_vault" "ia_key_vault" {
-  name                = "${local.vaultName}"
-  resource_group_name = "${local.vaultName}"
+  name                = "${local.key_vault_name}"
+  resource_group_name = "${local.key_vault_name}"
 }
 
 data "azurerm_key_vault_secret" "ia_case_api_url" {
@@ -16,24 +25,22 @@ data "azurerm_key_vault_secret" "ia_case_api_url" {
   vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
 }
 
-locals {
-  aseName             = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
-  previewVaultName    = "${var.raw_product}-aat"
-  nonPreviewVaultName = "${var.raw_product}-${var.env}"
-  vaultName           = "${(var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName}"
-}
-
 module "ia-apfr" {
-  source               = "git@github.com:contino/moj-module-webapp?ref=master"
+  source               = "git@github.com:hmcts/cnp-module-webapp?ref=master"
   product              = "${var.product}-${var.component}"
   location             = "${var.location}"
   env                  = "${var.env}"
   ilbIp                = "${var.ilbIp}"
+  resource_group_name  = "${local.resource_group_name}"
   is_frontend          = "${var.env != "preview" ? 1: 0}"
   subscription         = "${var.subscription}"
   additional_host_name = "${var.env != "preview" ? var.additional_hostname : "null"}"
   https_only           = "${var.env != "preview" ? "true" : "true"}"
-  common_tags          = "${var.common_tags}"
+  capacity             = "${var.capacity}"
+  instance_size        = "${var.instance_size}"
+  common_tags          = "${merge(var.common_tags, map("lastUpdated", "${timestamp()}"))}"
+  asp_name             = "${local.app_service_plan}"
+  asp_rg               = "${local.app_service_plan}"
 
   app_settings = {
     WEBSITE_NODE_DEFAULT_VERSION = "8.11.1"
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,13 +1,14 @@
 variable "product" {
-  type    = "string"
+  type = "string"
 }
 
 variable "raw_product" {
-  default = "ia" // jenkins-library overrides product for PRs and adds e.g. pr-123-ia
+  default = "ia"
+  // jenkins-library overrides product for PRs and adds e.g. pr-123-ia
 }
 
 variable "component" {
-  type   = "string"
+  type = "string"
 }
 
 variable "location" {
@@ -28,22 +29,18 @@ variable "subscription" {
   type = "string"
 }
 
-variable "ilbIp"{}
-
-variable "tenant_id" {
-  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
-}
-
-variable "client_id" {
-  description = "(Required) The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies. This is usually sourced from environment variables and not normally required to be specified."
-}
-
-variable "jenkins_AAD_objectId" {
-  type = "string"
-}
+variable "ilbIp" {}
 
 variable "common_tags" {
   type = "map"
+}
+
+variable "capacity" {
+  default = "1"
+}
+
+variable "instance_size" {
+  default = "I1"
 }
 
 variable "additional_hostname" {
