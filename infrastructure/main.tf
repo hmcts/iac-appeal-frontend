c102a228905d7fbba73e034e942ebed2c8d96479
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2018-10-01 13:09:01 +0100

    Renamed module to consistent format

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -27,7 +27,7 @@ data "azurerm_key_vault_secret" "ia_case_api_url" {
   vault_uri = "${data.azurerm_key_vault.ia_key_vault.vault_uri}"
 }
 
-module "ia-apfr" {
+module "ia_appeal_frontend" {
   source               = "git@github.com:hmcts/cnp-module-webapp?ref=master"
   product              = "${var.product}-${var.component}"
   location             = "${var.location}"
